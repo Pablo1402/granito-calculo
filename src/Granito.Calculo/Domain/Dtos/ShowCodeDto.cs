be845a803d3403b481b821249a003794cfef5f11
Pablo Barreto <drt.pbarreto@ifood3rd.com>
2023-04-16 14:06:38 -0300

    implementa endpoint showcode

---
+++ src/Granito.Calculo/Domain/Dtos/ShowCodeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dtos
+{
+    public class ShowCodeDto
+    {
+        public string UrlGitCalculo { get; set; }
+        public string UrlGitTaxa { get; set; }
+
+    }
+}
---
+++ src/Granito.Calculo/WebApi/Controllers/CodeController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace WebApi.Controllers
+{
+    public class CodeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("show-me-the-code")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(ShowCodeDto))]
+        public async Task<IActionResult> Index()
+        {
+            return Ok(new ShowCodeDto()
+            {
+                UrlGitCalculo = "https://github.com/Pablo1402/granito-calculo",
+                UrlGitTaxa= "https://github.com/Pablo1402/granito-taxas"
+            });
+        }
+
+
+    }
+
+    
+}
